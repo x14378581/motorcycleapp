7c01d421180b615199b018fa665ff83a35d98ca1
John Cahill <john-cahill1997@hotmail.com>
2017-12-03 01:31:04 +0000

    First commit

---
+++ app/src/main/java/com/example/john_/motorcycleapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.john_.motorcycleapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton showCameras = (ImageButton) findViewById(R.id.camerabtn);
+        ImageButton showFolders = (ImageButton) findViewById(R.id.folderbtn);
+        ImageButton showLocation = (ImageButton) findViewById(R.id.locationbtn);
+        showCameras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Cameras.class);
+                startActivity(intent);
+            }
+        });
+        showFolders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Folders.class);
+                startActivity(intent);
+            }
+        });
+
+        showLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Location.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
