0bf3250dfb9485f7b71de10223409595e17ecffa
John Cahill <john-cahill1997@hotmail.com>
2017-12-03 21:53:21 +0000

    All main pages completed in design and text added for raspberry pi

--- app/src/main/java/com/example/john_/motorcycleapp/MainActivity.java
+++ app/src/main/java/com/example/john_/motorcycleapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         showLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Location.class);
+                Intent intent = new Intent(getApplicationContext(),Location2.class);
                 startActivity(intent);
             }
         });
